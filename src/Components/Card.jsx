1d8ad38889ad2582fadcb5d748ed90dc8df25983
Sridhar <sridharmoturu@gmail.com>
2024-04-29 13:43:46 +0530

    url changed

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -5,13 +5,17 @@ const Card = ({ele,index}) => {
     <div className="w-2/3 mb-16 flex justify-center gap-4">
       <div>
         <h3 className="bg-primary p-3 rounded-xl text-white font-bold">
-          {index+1}
+          {index + 1}
         </h3>
       </div>
       <div>
         <h1 className="headings">{ele?.title} </h1>
         <p className="para">{ele?.subTitle}</p>
-        <img src={'http://localhost:4000'+ele?.image} alt="card" className="w-full" />
+        <img
+          src={`${import.meta.env.VITE_IMAGE_URL}`+ ele?.image}
+          alt="card"
+          className="w-full"
+        />
       </div>
     </div>
   );
