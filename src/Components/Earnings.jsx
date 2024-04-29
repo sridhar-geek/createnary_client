6760c0f3ddfafa323099075d9ed2c738a414992c
Sridhar <sridharmoturu@gmail.com>
2024-04-29 16:36:50 +0530

    almost done

--- src/Components/Earnings.jsx
+++ src/Components/Earnings.jsx
@@ -1,13 +1,34 @@
 import { useState } from "react";
-
-useState
+import {axiosInstance} from '../Services/api'
 const Earnings = () => {
-    const [progress, setProgress] = useState(10);
+  const [followers, setFollowers] = useState(100);
+  const [products, setProducts] = useState(20);
+  const [potentialIncome, setPotentialIncome] = useState(110000);
+  // keep track of followers
+  const updateFollowers = (newValue) => {
+    setFollowers(newValue);
+  };
+  // keep track of products
+  const updateProducts = (newValue) => {
+    setProducts(newValue);
+  };
 
-    // Update the progress value (this could be tied to an event or state change)
-    const updateProgress = (newValue) => {
-      setProgress(newValue);
-    };
+  const formaterFun = (number) => {
+    const formatter = new Intl.NumberFormat("en-IN", {
+      style: "currency",
+      currency: "INR",
+    });
+    return formatter.format(number);
+  }
+
+  // send post request to server and get Potential Income
+  const calculateIncome = async (e) => {
+    e.preventDefault();
+    const data = { followers, products };
+    const income = await axiosInstance.post("/calculate", data);
+    console.log(income.data)
+    setPotentialIncome(income.data)
+  };
   return (
     <div className="m-10">
       <h3 className="headings md:text-center  font-semibold mb-2 md:mb-10">
@@ -16,7 +37,7 @@ const Earnings = () => {
       </h3>
       <div className="flex flex-col md:flex-row md:items-center justify-evenly">
         {/* combination div for select tag and progress bar */}
-        <div className="flex flex-col gap-6 ">
+        <div className="flex flex-col gap-6  ">
           <span className="questions">What kind of influencer are you?</span>
           {/* select tag */}
           <div>
@@ -34,55 +55,59 @@ const Earnings = () => {
           </div>
           {/* Progress bar starts*/}
           <span className="questions ">How many followers do you have?</span>
-          <div className="text-primary">
+          <div className="p-4">
             <input
               type="range"
               min="10"
               max="1000"
               step="10"
-              value={progress}
-              onChange={(e) => updateProgress(parseInt(e.target.value, 10))}
+              value={followers}
+              onChange={(e) => updateFollowers(parseInt(e.target.value, 10))}
               className="w-full"
             />
           </div>
+          <div className="follower_products">{`${followers}K`}</div>
           <span className="questions">
             How many products do you list monthly?
           </span>
-          <div>
-            <div className="p-4">
-              <input
-                type="range"
-                min="10"
-                max="1000"
-                step="10"
-                value={progress}
-                onChange={(e) => updateProgress(parseInt(e.target.value, 10))}
-                className="w-full mt-4"
-              />
-            </div>{" "}
-          </div>
+          <div className="p-4">
+            <input
+              type="range"
+              min="1"
+              max="50"
+              step="1"
+              value={products}
+              onChange={(e) => updateProducts(parseInt(e.target.value, 10))}
+              className="w-full "
+            />
+          </div>{" "}
           {/* Progress bar ends*/}
+          <div className="follower_products rounded-2xl">{products}</div>
         </div>
         {/* calculate button for small screens */}
         <div className="text-center">
-          <button className="text-3xl md:hidden">Calculate</button>
+          <button onClick={calculateIncome} className="btn md:hidden mt-6">
+            Calculate
+          </button>
         </div>
         {/* monthly and early earnings starts */}
         <div className="md:mt-28 flex flex-row md:flex-col gap-8 items-center justify-around">
-          <div>
-            <h3>Monthly earings</h3>
-            <h2>₹ 2333,000</h2>
+          <div className="flex items-center flex-col">
+            <h3 className="headings">Monthly Earings</h3>
+            <h2>{formaterFun(potentialIncome)}</h2>
           </div>
-          <div>
-            <h3>Yearly earings</h3>
-            <h2>₹ 2333,000</h2>
+          <div className="flex items-center flex-col">
+            <h3 className="headings">Yearly Earings</h3>
+            <h2>{formaterFun(potentialIncome * 12)}</h2>
           </div>
         </div>
         {/* monthly and early earnings starts */}
       </div>
       {/* calculate button for large screens */}
       <div className="flex justify-center">
-        <button className="text-3xl hidden md:block">Calculate</button>
+        <button onClick={calculateIncome} className="btn hidden md:block">
+          Calculate
+        </button>
       </div>
     </div>
   );
--- src/Components/ImageSlider.jsx
+++ src/Components/ImageSlider.jsx
@@ -5,6 +5,32 @@ import image6 from "../../public/image6.png";
 import image8 from "../../public/image8.png";
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 
+const RightButton = ({ onClick, ...rest }) => {
+  return (
+    <button
+      onClick={onClick}
+      {...rest}
+      style={{
+        display: "inline-block",
+        background: "none",
+        border: "none",
+        padding: "10px",
+        color: "#000",
+      }}
+    >
+      {" "}
+      <MdKeyboardArrowRight />
+    </button>
+  );
+};
+
+const LeftButton = ({ onClick, ...rest }) => {
+  return (
+    <button onClick={onClick} {...rest}>
+      <MdKeyboardArrowLeft />
+    </button>
+  );
+};
 
 const responsive = {
   desktop: {
@@ -22,32 +48,32 @@ const responsive = {
 };
 
 const Slider = () => {
-    const images = [image5, image6, image8];
+  const images = [image5, image6, image8];
   return (
-   <Carousel
-     swipeable={true}
-     draggable={true}
-     showDots={false}
-     autoPlay={true}
-     responsive={responsive}
-     infinite={false}
-     autoPlaySpeed={1000}
-     keyBoardControl={true}
-     transitionDuration={500}
-     containerClass="carousel-container"
-     dotListClass="custom-dot-list-style"
-     itemClass="carousel-item-padding-40-px"
-   >
-     {images.map((image, index) => (
-      <div key={index} className="w-2/3  mx-auto">
-        <img  src={image} alt="group" className="w-full" />
-      </div>
-     ))}
-   </Carousel>
-  )
-}
+    <Carousel
+      swipeable
+      draggable
+      // customLeftArrow={<LeftButton />}
+      // customRightArrow={<RightButton />}
+      showDots={false}
+      responsive={responsive}
+      ssr
+      keyBoardControl
+      transitionDuration={500}
+      containerClass="carousel-container"
+      dotListClass="custom-dot-list-style"
+      itemClass="carousel-item-padding-40-px"
+    >
+      {images.map((image, index) => (
+        <div key={index} className="w-2/3  mx-auto">
+          <img src={image} alt="group" className="w-full" />
+        </div>
+      ))}
+    </Carousel>
+  );
+};
 const ImageSlider = () => {
-      const handleClick = () => {};
+  const handleClick = () => {};
   return (
     <div className="text-center bg-primary p-3">
       <div>
@@ -58,7 +84,6 @@ const ImageSlider = () => {
         <Slider />
         <button className="btn bg-white text-primary">Try for Free</button>
       </div>
-
     </div>
   );
 };
